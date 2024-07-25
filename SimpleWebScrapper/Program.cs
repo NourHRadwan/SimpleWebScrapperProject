a3e3ddf7fe0342c386804b60a4a5857bbb509f04
Nour Hossam <nourhradwan95@gmail.com>
2024-07-25 11:52:39 +0100

    Scraper Class to implement basic scraping and handle Granular Scraping

--- SimpleWebScrapper/Program.cs
+++ SimpleWebScrapper/Program.cs
@@ -1,31 +1,98 @@
-﻿namespace SimpleWebScrapper
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using SimpleWebScrapper.Data;
+using SimpleWebScrapper.Workers;
+using System.Runtime.CompilerServices;
+using SimpleWebScrapper.Builders;
+
+namespace SimpleWebScrapper
 {
     internal class Program
     {
+        #region Ask the user for input
+        // <summary>
+        // Gets user input for the city to scrape data from.
+        // </summary>
+        // <returns>The city name entered by the user.</returns>
+
+        static string GetCityInput()
+        {
+            Console.WriteLine("Enter the city you want to scrape data from: ");
+            return Console.ReadLine() ?? "";
+        }
+
+        // <summary>
+        // Gets user input for the category to scrape data from.
+        // </summary>
+        // <returns>The category name entered by the user.</returns>
+        static string GetCategoryInput()
+        {
+            Console.WriteLine("Enter the category you want to scrape data from: ");
+            return Console.ReadLine() ?? "";
+        }
+        #endregion
+
+        #region Content Download
+        // <summary>
+        // Downloads the HTML content of the specified URL Page.
+        // </summary>
+        // <param name="city"> The city to scrape data from.</param>
+        // <param name="category"> The category to scrape data from.</param>
+        // <returns>The downloaded HTML content of the specified URL Page.</returns>
+
+        static string DownloadContent(string city, string category)
+        {   
+            using (WebClient client = new WebClient())
+            {
+                try {
+                    return client.DownloadString($"https://{city.Replace(" ", string.Empty)}.craigslist.org/search/{category}");
+                    // string.Empty to handle the case where the city name has a space in it Like New York.
+                }
+                catch (WebException ex)
+                {
+                    Console.WriteLine($"Error Downloading Content: {ex.Message}");
+                    return "";
+                }
+                
+             }
+        }
+        #endregion
+
+        #region ScrapeCrieteria Creation
+        // <summary>
+        // Creates a scrape criteria object based on the provided content and regex pattern.
+        // </summary>
+        // <param name="content"> The HTML content to scrape data from.</param>
+        // <param name="regexpattern"> The regualr expression pattern to use for scraping data.</param>
+        // <returns>The scrape criteria object created based on the provided content and regex pattern.</returns>
+        static ScrapeCriteria CreateScrapeCriteria(string content, string regexpattern)
+        {
+            ScrapeCriteria scrapeCriteria = new ScraperCriteriaBuilder()
+                .WithData(content)
+                .WithRegex(regexpattern)
+                .WithRegexOption(RegexOptions.ExplicitCapture) // ExplicitCapture to ignore the unnamed groups in the regex pattern.
+                
+                //creating object of scrapping criteria part builder inside the scraper criteria builder.
+                .WithParts(new ScrapeCriteriaPartBuilder()
+                    .WithRegex(@">(.*?)</a>")
+                    .WithRegexOption(RegexOptions.Singleline)
+                    .Build())
+                .WithParts(new ScrapeCriteriaPartBuilder()
+                    .WithRegex(@"href=\""(.*?\""")
+                    .WithRegexOption(RegexOptions.Singleline)
+                    .Build())
+                .Build();
+            return scrapeCriteria;
+        }
+        #endregion
         static void Main(string[] args)
         {
-
-
-            #region Part 1: Builder Pattern
-            //First step is to create two builders that are going to be help us create Search Criteria
-            //and it is going to make it alot more flexable to change the search criteria
-
-            ///Problem:
-            ///We have a class called ScrapeCriteria that has a lot of properties that are required to be set
-            ///in order to create a ScrapeCriteria object. The problem is that we have to set all of the properties
-            ///at the same time and we have to set them in the correct order. If we don't set them in the correct order
-            ///we are going to get an error. This is a problem because we have to remember the order of the properties
-            ///and we have to remember to set all of the properties.
-            ///Solution:
-            ///We are going to create a builder that is going to help us create a ScrapeCriteria object.
-
-            #region Required Data
-            //The data that is required to create a builder is the following
-
-
-            #endregion
-
-            #endregion
         }
     }
 }
---
+++ SimpleWebScrapper/Workers/Scraper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using SimpleWebScrapper.Data;
+
+namespace SimpleWebScrapper.Workers
+{
+    internal class Scraper
+    {
+        /// <summary>
+        /// Scraps data from a given websites based on the provided ScraperCriteria
+        /// </summary>
+        /// <param name="scrapeCriteria"> The criteria defining how to scrape the data.</param>
+        /// <returns> A List if strings containing the exteracted data elements</returns>
+        public List<string> Scrape(ScrapeCriteria scrapeCriteria)
+        {
+            //List to store the scraped elements
+            List<string> scrapedElements = new List<string>();
+
+            //Preform the Scraping Operation
+            try
+            {
+                //Match the data against the provided regular expression
+                MatchCollection matches = Regex.Matches(scrapeCriteria.Data, scrapeCriteria.Regex, scrapeCriteria.RegexOption);
+
+                //Iterate through each match
+                foreach (Match match in matches)
+                {
+                    // Check if the scrape criteria specefies parts to be extracted
+                    if (!scrapeCriteria.Parts.Any())
+                    {
+                        //If no parts are specified, extract the entire match
+                        scrapedElements.Add(match.Groups[0].Value);
+                    }
+                    else
+                    {
+                        //Parts are specified, extract the data within the match
+                        foreach (var part in scrapeCriteria.Parts)
+                        {
+                            //Match the part's regular expression against the current match
+                            Match matchedPart = Regex.Match(match.Groups[0].Value, part.Regex, part.RegexOption);
+
+                            //Check if the match was found
+                            if (matchedPart.Success)
+                            {
+                                //Add the matched part to the list of scraped elements
+                                scrapedElements.Add(matchedPart.Groups[1].Value);
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //Log the Exeption for debugging purposes
+                Console.WriteLine($"Error During Scraping: {ex.Message}");
+            }
+            //Return the scraped elements list
+            return scrapedElements;
+
+        }
+    }
+}
