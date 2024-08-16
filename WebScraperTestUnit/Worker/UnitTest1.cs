07a29d52f8aa2cc1af4dd8710c61aba6961dbd07
Nour Hossam <nourhradwan95@gmail.com>
2024-08-16 17:58:19 +0100

    Add XML_Style comments to the Unit test to describe the purpose of each test

--- WebScraperTestUnit/Worker/UnitTest1.cs
+++ WebScraperTestUnit/Worker/UnitTest1.cs
@@ -7,16 +7,19 @@ using SimpleWebScrapper.Data;
 
 namespace WebScraperTestUnit.Worker
 {
+    /// <summary>
+    ///  Sets up the tests for the Scraper class.
+    /// </summary>
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
+     
+        /// <summary>
+        /// Test that the Scraper can extract a single element with a single part.
+        /// </summary>
         [Test]
         public void Test1()
         {
+            // Arrange
             var content = "Some fluff data <a href=\"http://domain.com\" data-id=\"someId\" class=\"result-title hdrlnk\">some text</a> more fluff data";
 
             ScrapeCriteria scrapeCriteria = new ScraperCriteriaBuilder()
@@ -24,36 +27,47 @@ namespace WebScraperTestUnit.Worker
                 .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
                 .WithRegexOption(RegexOptions.ExplicitCapture)
                 .Build();
+
+            // Act
             var scraper = new Scraper();
             var foundElements = scraper.Scrape(scrapeCriteria);
 
+            // Assert
             Assert.That(foundElements.Count, Is.EqualTo(1));
             Assert.That(foundElements[0], Is.EqualTo("<a href=\"http://domain.com\" data-id=\"someId\" class=\"result-title hdrlnk\">some text</a>"));
         }
 
 
+        /// <summary>
+        /// Test that the Scraper can extract multiple elements with multiple parts.
+        /// </summary>
+
         [Test]
         public void Test2()
         {
+            // Arrange
             var content = "Some fluff data <a href=\"http://domain.com\" data-id=\"someId\" class=\"result-title hdrlnk\">some text</a> more fluff data";
 
             ScrapeCriteria scrapeCriteria = new ScraperCriteriaBuilder()
-                .WithData(content)
-                .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
-                .WithRegexOption(RegexOptions.ExplicitCapture)
-                .WithPart(new ScrapeCriteriaPartBuilder()
-                    .WithRegex(@">(.*?)</a>")
-                    .WithRegexOption(RegexOptions.Singleline)
-                    .Build())
-                .WithPart(new ScrapeCriteriaPartBuilder()
-                    .WithRegex(@"href=\""(.*?)\""")
-                    .WithRegexOption(RegexOptions.Singleline)
-                    .Build())
-                .Build();
+                  .WithData(content)
+                  .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                  .WithRegexOption(RegexOptions.ExplicitCapture)
+                  .WithParts(new ScrapeCriteriaPartBuilder()
+                      .WithRegex(@">(.*?)</a>")
+                      .WithRegexOption(RegexOptions.Singleline)
+                      .Build())
+                  .WithParts(new ScrapeCriteriaPartBuilder()
+                      .WithRegex(@"href=\""(.*?)\""")
+                      .WithRegexOption(RegexOptions.Singleline)
+                      .Build())
+                  .Build();
 
 
+            // Act
             var scraper = new Scraper();
             var foundElements = scraper.Scrape(scrapeCriteria);
+
+            // Assert
             Assert.That(foundElements.Count, Is.EqualTo(2));
             Assert.That(foundElements[0], Is.EqualTo("some text"));
             Assert.That(foundElements[1], Is.EqualTo("http://domain.com"));
